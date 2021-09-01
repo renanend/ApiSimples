4fa61549f64d5e524ab601134897a52ec82aa7db
renan silva <renanend@hotmail.com>
2021-08-31 23:03:13 -0300

    Add project files.

---
+++ WOW.Api.Webservice/AmazonApi.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOW.Api.Webservice
+{
+    public class AmazonApi
+    {
+        public static string RetornaTexto()
+        {
+            string URI = "http://ec2-52-91-68-80.compute-1.amazonaws.com:8000/api/test";
+            string response = string.Empty;
+            string request = JsonConvert.SerializeObject(new Request() { Nome = "Daniel" });
+            using (WebClient wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/json";
+                response = wc.UploadString(URI, "POST", request);
+            }
+            return response;
+        }
+    }
+}
---
+++ WOW.Api.Webservice/Request.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WOW.Api.Webservice
+{
+    public class Request
+    {
+        [JsonProperty("nome")]
+        public string Nome { get; set; }
+    }
+}
